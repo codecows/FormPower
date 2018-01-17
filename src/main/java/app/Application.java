af87f3a88ac0af3b9bf92068de2b0f2c3d734d58
Jack <57134898@qq.com>
2018-01-17 15:01:44 +0800

    1

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -9,5 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.printf("                               +---------------------------------------------+" + System.getProperty("line.separator"));
+        System.out.printf("                               |           ---       启动成功     ---         |" + System.getProperty("line.separator"));
+        System.out.printf("                               +---------------------------------------------+" + System.getProperty("line.separator"));
+
     }
 }
