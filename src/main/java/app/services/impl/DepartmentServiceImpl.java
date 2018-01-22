72ad6e34f31f0e2c03022744cd701a1f0225ff62
Jack <57134898@qq.com>
2018-01-22 10:18:25 +0800

    JWT INIT

--- src/main/java/app/services/impl/DepartmentServiceImpl.java
+++ src/main/java/app/services/impl/DepartmentServiceImpl.java
@@ -1,7 +1,16 @@
 package app.services.impl;
 
+import app.model.Department;
+import app.services.DepartmentService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DepartmentServiceImpl {
+public class DepartmentServiceImpl implements DepartmentService{
+    @Override
+    public List<Department> getDepts() {
+        //TODO 未实现
+        return null;
+    }
 }
