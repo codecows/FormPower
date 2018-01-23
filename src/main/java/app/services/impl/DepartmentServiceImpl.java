d6e0719f6dff81cf30e66e27a966e9ed10d8cce3
Jack <57134898@qq.com>
2018-01-24 00:29:37 +0800

    处理冲突

--- src/main/java/app/services/impl/DepartmentServiceImpl.java
+++ src/main/java/app/services/impl/DepartmentServiceImpl.java
@@ -22,11 +22,9 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Resource
     private DepartmentConverter departmentConverter;
 
-<<<<<<< HEAD
     //@Resource todo 没有 @Component 注解的 不能 用自动注入标签 需要new
     private SysDepartmentExample sysDepartmentExample;
-=======
->>>>>>> b7d98b4f901523cd5ef93479003e66c58bafbe4c
+
 
     @Override
     public List<Department> getDepts() {
