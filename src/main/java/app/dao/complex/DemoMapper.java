b997d5eb186a14aaa18e2e5e1283b4c95d79e2ca
Jack <57134898@qq.com>
2018-01-26 22:44:15 +0800

    mybatis 一对一 一对多 demo

--- src/main/java/app/dao/complex/DemoMapper.java
+++ src/main/java/app/dao/complex/DemoMapper.java
@@ -1,30 +1,45 @@
 package app.dao.complex;
 
+import app.dao.entities.SysMenu;
+import app.dao.entities.TestMenus;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.mapping.FetchType;
 import org.apache.ibatis.type.JdbcType;
+import springfox.documentation.service.ApiListing;
 
 import java.util.List;
 
 public interface DemoMapper {
-    DemoEntity getUserInfo();
-
     @Select({
             "select",
             "uid, uname, deptid",
-            "from test_users"
+            "from test_users",
+            "where uid = #{userid}"
     })
     @Results({
-            @Result(column="uid", property="uid", jdbcType= JdbcType.VARCHAR, id=true),
-            @Result(column="uname", property="uname", jdbcType=JdbcType.VARCHAR),
-            @Result(column="deptid", property="testDept", jdbcType=JdbcType.VARCHAR,
-                    one = @One(select = "app.dao.mappers.TestDeptMapper.selectByPrimaryKey",fetchType= FetchType.EAGER)
+            @Result(column = "uid", property = "uid", jdbcType = JdbcType.VARCHAR, id = true),
+            @Result(column = "uname", property = "uname", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "deptid", property = "testDept", jdbcType = JdbcType.VARCHAR,
+                    one = @One(select = "app.dao.mappers.TestDeptMapper.selectByPrimaryKey", fetchType = FetchType.EAGER)
             )
             ,
-            @Result(column="userid", property="testMenus", jdbcType=JdbcType.VARCHAR,
-                    many = @Many(select = "select * from test_menus where menuid = #{userid}",fetchType= FetchType.EAGER)
+            @Result(column = "uid", property = "testMenus", jdbcType = JdbcType.VARCHAR,
+                    many = @Many(select = "getMenusByUser", fetchType = FetchType.EAGER)
             )
     })
-    List<DemoEntity> getUserInfos();
+    DemoEntity getUserInfo(String userId);
+
+    @Select({
+            "select",
+            "menuid, menuname, userid",
+            "from test_menus",
+            "where userid = #{userid}"
+    })
+    @Results({
+            @Result(column = "menuid", property = "menuid", jdbcType = JdbcType.VARCHAR, id = true),
+            @Result(column = "menuname", property = "menuname", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "userid", property = "userid", jdbcType = JdbcType.VARCHAR)})
+    List<TestMenus> getMenusByUser(@Param("userid") String useid);
+
 
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,6 +1,8 @@
 import app.Application;
 import app.dao.complex.DemoEntity;
 import app.dao.complex.DemoMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +19,14 @@ public class test {
 
     @Test
     public void test1() {
-        List<DemoEntity> userInfos = demoMapper.getUserInfos();
-        assert userInfos.size() > 0 : "集合size应该大于0";
+        DemoEntity userInfo = demoMapper.getUserInfo("001");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String s = objectMapper.writeValueAsString(userInfo);
+            System.out.println(s);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        assert userInfo != null : "集合size应该大于0";
     }
 }
