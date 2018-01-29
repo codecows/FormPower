873ea560bb68155c7bff7c2cc195a0f0f25dff5a
Jack <57134898@qq.com>
2018-01-29 22:02:21 +0800

    新增业务模型

--- src/main/java/app/temp/pojo/DeptPojo.java
+++ src/main/java/app/temp/pojo/DeptPojo.java
@@ -1,4 +1,40 @@
 package app.temp.pojo;
 
 public class DeptPojo {
+    private String deptId;
+    private String deptName;
+    private String parId;
+    private String status;
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getParId() {
+        return parId;
+    }
+
+    public void setParId(String parId) {
+        this.parId = parId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
