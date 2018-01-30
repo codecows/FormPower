7d4c6546c2bd4c73e2348617455acb30d78352d9
ronggangfan <ronggangfan@users.noreply.github.com>
2018-01-30 21:30:36 +0800

    更新下代码

---
+++ src/main/java/app/configs/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package app.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by real on 2018/1/30.
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    //todo 安全模块配置文件，未完
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+
+}
