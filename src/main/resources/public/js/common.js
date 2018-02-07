5a29f2762bc870f26c6796cdf76edea1070dbf90
Jack <57134898@qq.com>
2018-02-07 13:40:09 +0800

    ajax helper

--- src/main/resources/public/js/common.js
+++ src/main/resources/public/js/common.js
@@ -1,18 +1,33 @@
-function ajax(u,t,d ,s) {
-    $.ajax({
-        url: u,
-        type: t,
-        data: d,
-        timeout: 5000,
-        dataType: 'json',
-        headers: {
-            "x-access-token": "Bearer " + localStorage.getItem("token"),
-            "visitor": localStorage.getItem("visitor")
+var HttpHelper = function () {
+    return {
+        ajax: function (url, type, data, sucFun) {
+            if (!type) type = "GET";
+            if (!data) data = {};
+            if ((!url) || !(sucFun)) return;
+            $.ajax({
+                url: url,
+                type: type,
+                data: data,
+                timeout: 5000,
+                dataType: 'json',
+                headers: {
+                    "x-access-token": "Bearer " + localStorage.getItem("token"),
+                    "visitor": localStorage.getItem("visitor")
+                },
+                contentType: 'application/json; charset=UTF-8',
+                success: sucFun,
+                error: function (e) {
+                    alert(JSON.stringify(e));
+                }
+            });
         },
-        contentType: 'application/json; charset=UTF-8',
-        success: s,
-        error:function (e) {
-            layer.msg(JSON.stringify(e));
+        method: function () {
+            return {
+                GET: "GET",
+                POST: "POST",
+                PUT: "PUT",
+                DELETE: "DELETE"
+            };
         }
-    });
-}
+    };
+}();
