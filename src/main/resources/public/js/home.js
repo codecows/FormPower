c82a473def34b228592c1e5cfa3bca93fcf7660b
Jack <57134898@qq.com>
2018-02-10 01:04:26 +0800

    系统 与 菜单实现

--- src/main/resources/public/js/home.js
+++ src/main/resources/public/js/home.js
@@ -1,6 +1,7 @@
 var MainMenu = function () {
     return {
         loadModule: function () {
+            var ico = "fa fa-book";//todo 需要替换
             if (window.UserInfo === null) return;
             var sysMenus = window.UserInfo.systemMenus;
 
@@ -10,7 +11,7 @@ var MainMenu = function () {
             $.each(sysMenus, function (index, item) {
                 var ico = "fa fa-book";//todo 需要替换
                 var _tempHtml = "<div menuId='" + item.menuId + "' class=\"top-news\">\n" +
-                    "       <a href=\"#\" class=\"btn blue-hoki active\">\n" +
+                    "       <a href=\"javascript:;\" class=\"btn blue-hoki active\">\n" +
                     "       <span>" + item.menuName + "</span>";
 
                 if (item.remark) {
@@ -39,6 +40,49 @@ var MainMenu = function () {
             // alert(html);
             if (html.length <= 0) return
             $(".news-page div.row").append(html);
+
+            $(".news-page div.row").on('click', '.top-news', function (e) {
+                var the = $(this);
+                var hasItem = false;
+                $.each(sysMenus, function (index, item) {
+                    if (item.menuId === the.attr("menuId")) {
+                        var menuHtml = "";
+                        $.each(item.groupMenuList, function (index1, item1) {
+                            menuHtml += "<li>\n" +
+                                "    <a href=\"javascript:;\">\n" +
+                                "        <i class=\"" + ico + "\"></i>\n" +
+                                "        <span class=\"title\">" + item1.menuName + "</span>\n" +
+                                "        <span class=\"selected\"></span>\n" +
+                                "        <span class=\"arrow\"></span>\n" +
+                                "    </a>\n" +
+                                "    <ul class=\"sub-menu\">";
+                            $.each(item1.menuList, function (index2, item2) {
+                                menuHtml += "<li>\n" +
+                                    "    <a class='ajaxify' href=\"" + item2.menuUrl + "\">\n" +
+                                    "        <i class=\"" + ico + "\"></i>" + item2.menuName + "</a>\n" +
+                                    "    </li>";
+                            });
+                            menuHtml += "</ul></li>";
+                        });
+                        if (menuHtml.length <= 0) return false;
+                        hasItem = true;
+                        $(".page-sidebar-menu > li:not(.start,.last,.sidebar-search-wrapper)").remove();
+                        $(menuHtml).insertAfter($(".page-sidebar-menu li.start"));
+                        var subMenu = $(".page-sidebar-menu > li:not(.start,.last,.sidebar-search-wrapper):first").find("ul > li > a:first");
+                        if (subMenu.length > 0) {
+                            subMenu.click();
+                        }else{
+                            layer.msg("没有可用菜单~");
+                        }
+                        return false;
+                    }
+                });
+                if (!hasItem) {
+                    layer.msg("系统没有子模块~");
+                }
+                e.preventDefault();
+            });
+
         },
         loadMenus: function () {
             if (window.UserInfo === null) return;
