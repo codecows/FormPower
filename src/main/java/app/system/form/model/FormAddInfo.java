7fa2691d16b16ac20f0324502249d7cd2fd44e53
Jack <57134898@qq.com>
2018-03-02 16:10:25 +0800

    dev

--- src/main/java/app/system/form/model/FormAddInfo.java
+++ src/main/java/app/system/form/model/FormAddInfo.java
@@ -7,12 +7,27 @@ import app.base.model.BaseBizModel;
  * 新建一个表单后 form 表里新增一条记录
  * 然后网数据库新增一个表 表名为id 属性
  * 生成的表 采用统一前缀  比如 u_table1
+ * 默认生成 一个存储过程
+ * 参数有
+ * 表单ID
+ * 操作人
+ * 操作时间
+ * 操作类型 增删改 ADD/DEL/UPD
+ * <p>
+ * 无返回值
  */
 public class FormAddInfo extends BaseBizModel {
     //表名
     private String code;
     private String name;
     private String remark;
+    //js内容
+    private String script;
+    //函数名
+    private String funcName;
+    //函数
+    private String funcBody;
+    private String status;
 
     public String getId() {
         return code;
